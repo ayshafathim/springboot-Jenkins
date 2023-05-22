2c6c2d38aeef3e80bcaa973fa0fb41715b4b5b14
aysha <ayshasharaf2000@gmail.com>
2023-05-22 13:12:17 +0530

    second commit

--- src/main/java/com/ust/springboot/SpringbootApplication.java
+++ src/main/java/com/ust/springboot/SpringbootApplication.java
@@ -2,10 +2,16 @@ package com.ust.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringbootApplication {
-
+@GetMapping("/Hello")
+public String welcome(){
+	return "Hello World";
+}
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootApplication.class, args);
 	}
